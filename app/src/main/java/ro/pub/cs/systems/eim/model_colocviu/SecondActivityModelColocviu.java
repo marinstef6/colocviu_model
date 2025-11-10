4ea5ab86631004344b2113e98f27e94d1542c524
marinstef6 <132059219+marinstef6@users.noreply.github.com>
2025-11-10 09:50:59 +0200

    Implement button click handling in SecondActivity

--- app/src/main/java/ro/pub/cs/systems/eim/model_colocviu/SecondActivityModelColocviu.java
+++ app/src/main/java/ro/pub/cs/systems/eim/model_colocviu/SecondActivityModelColocviu.java
@@ -1,6 +1,11 @@
 package ro.pub.cs.systems.eim.model_colocviu;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,15 +15,62 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class SecondActivityModelColocviu extends AppCompatActivity {
 
+    private EditText numberofClick;
+    private Button register, cancel;
+    private ButtonClickListener buttonClickListener = new ButtonClickListener();
+
+    private class ButtonClickListener implements View.OnClickListener {
+        @Override
+        public void onClick(View view) {
+            if (view.getId() == R.id.register_button)
+                setResult(RESULT_OK, null);
+            else if (view.getId() == R.id.cancel_button)
+                setResult(RESULT_CANCELED, null);
+            finish();
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+        //EdgeToEdge.enable(this);
         setContentView(R.layout.activity_second_model_colocviu);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+        numberofClick = (EditText) findViewById(R.id.editTextText2);
+        Intent intent = getIntent();
+        if (intent != null && intent.getExtras().containsKey(Constants.NUMBER_OF_CLICKS)) {
+            int numberOfClicks = intent.getIntExtra(Constants.NUMBER_OF_CLICKS, -1);
+            numberofClick.setText(String.valueOf(numberOfClicks));
+        }
+
+        register = (Button) findViewById(R.id.register_button);
+        register.setOnClickListener(buttonClickListener);
+        cancel = (Button) findViewById(R.id.cancel_button);
+        register.setOnClickListener(v -> {
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("pressedButton", "result");
+            setResult(RESULT_OK, resultIntent);
+            finish();
         });
+
+        cancel.setOnClickListener(v -> {
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("pressedButton", "Cancel");
+            setResult(RESULT_CANCELED, resultIntent);
+            finish();
+        });
+//daca vreau sa afisez ceva
+//        register.setOnClickListener(v -> {
+//            Intent resultIntent = new Intent();
+//            String value = numberofClick.getText().toString();
+//            resultIntent.putExtra("pressedButton", value);
+//            setResult(RESULT_OK, resultIntent);
+//            finish();
+//        });
     }
 }
